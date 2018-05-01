db2fc45bf55d4f661cfd65f2c700ff89b6a1a45f
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-05-01 17:07:24 +0200

    switched to GPLv3

--- Bits.cs
+++ Bits.cs
@@ -5,14 +5,14 @@
  ***************************************************************************/
 
 /***************************************************************************
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License version 2.1 as published by the Free Software Foundation.
- 
-    This library is distributed in the hope that it will be useful,
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License v3 as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-    Lesser General Public License for more details.
+    General Public License v3 for more details.
  ***************************************************************************/
 /**
    @file Bits.cs
